8aafa7ac7606e7e518b9838f093c239d5484e465
seojaehong <woghd8614@daum.net>
2021-10-08 11:50:05 +0900

    단위테스트 리팩터링

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,7 @@ const expressHandlebars = require('express-handlebars'); //view 처리용
 const app = express();
 const port = process.env.PORT || 3000;
 const fortunes = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 // const fortunes = ['아는 것이 힘이다', '모르는게 약이다'];
 //엔진 설정
@@ -16,26 +17,34 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    // const randomfortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    // res.render('about', { fortune: randomfortune });
-    res.render('about', { fortune: fortunes.getFortune() });
-});
+// app.get('/about', (req, res) => {
+//     // const randomfortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+//     // res.render('about', { fortune: randomfortune });
+//     res.render('about', { fortune: fortunes.getFortune() });
+// });
 
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.get('/about', handlers.about);
 
-app.use((err, req, res, next) => {
-    console.log(err.message);
-    res.status(500);
-    res.render('500');
-});
+// app.use((req, res) => {
+//     res.status(404);
+//     res.render('404');
+// });
+
+app.use(handlers.notFound);
+
+// app.use((err, req, res, next) => {
+//     console.log(err.message);
+//     res.status(500);
+//     res.render('500');
+// });
+
+app.use(handlers.serverError);
+
 app.listen(port, () => {
     console.log(`포트 ${port}`);
 });
